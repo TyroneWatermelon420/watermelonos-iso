5027d251b16a668b0b8902cbb2ebc2e14356b818
TyroneWatermelon420 <tyronewatermlon@tutanota.com>
2023-10-13 19:55:44 +0000

    Add customization

--- 2023.10/releng/profiledef.sh
+++ 2023.10/releng/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="watermelonos"
+iso_label="WATERMELONOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Tyrone Watermelon <https://github.com/TyroneWatermelon420>"
+iso_application="WatermelonOS Live/Rescue CD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
